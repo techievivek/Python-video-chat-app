4458039cb428c388a15f2f3c8706d279ad170834
Techie Vivek <35863227+techievivek@users.noreply.github.com>
2019-11-14 01:53:26 +0530

    Upload client and server.py

---
+++ client.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost',8089))
+
+while True:
+    ret,frame=cap.read()
+    # Serialize frame
+    data = pickle.dumps(frame)
+
+    # Send message length first
+    message_size = struct.pack("L", len(data)) ### CHANGED
+
+    # Then data
+    clientsocket.sendall(message_size + data)
